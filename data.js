4e53e5c08a1a9c4ba75971519b7f96686b923936
RC <125175307+123456dr@users.noreply.github.com>
2024-03-19 14:08:59 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,7 @@
-var cleanedData = `t=7 h=7
-t=23 h=43
-t=45 h=24
-t=24 h=64
-t=0 h=0 `;
+var cleanedData = `t=0 h=0 
+    t=0 h=0 t=0 h=0 
+    t=0 h=0 t=0 
+    h=0 `;
     const  myData= cleanedData.replace(/\n/g, ''); // 移除所有換行符
     
 
