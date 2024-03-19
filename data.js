a40ae3b9337eff2fd8ad9e26c43fc101370cd440
RC <125175307+123456dr@users.noreply.github.com>
2024-03-19 13:55:49 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+var cleanedData = `t=0 h=65
+t=23 h=43
+t=45 h=241
+t=24 h=64
+t=525 h=4 `;
+    const  myData= cleanedData.replace(/\n/g, ''); // 移除所有換行符
+    
+
+
+// 正則表達式來匹配第一個 t 和 h 值
+const firstTempMatch = myData.match(/t=(\d+)/);
+const firstHumidMatch = myData.match(/h=(\d+)/);
+
+let firstTemp, firstHumid;
+
+// 如果匹配成功，則提取值
+if (firstTempMatch) {
+    firstTemp = parseInt(firstTempMatch[1]);
+}
+
+if (firstHumidMatch) {
+    firstHumid = parseInt(firstHumidMatch[1]);
+}
+
+// 正則表達式來匹配最後一個 t 值
+const lastTempMatch = myData.match(/t=(\d+)(?!.*t)/g);
+const lastHumidMatch = myData.match(/h=(\d+)(?!.*h)/g);
+
+let lastTemp, lastHumid;
+
+// 如果匹配成功，則提取值
+if (lastTempMatch) {
+    lastTemp = parseInt(lastTempMatch.pop().split('=')[1]);
+}
+
+if (lastHumidMatch) {
+    lastHumid = parseInt(lastHumidMatch.pop().split('=')[1]);
+}
+
+// 計算溫度和濕度變化量
+const tempChange = lastTemp - firstTemp;
+const humidChange = lastHumid - firstHumid;
+
+
